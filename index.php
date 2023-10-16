fc7c8512a48acc31ecae584985cf8f701d0ce87c
Pol <pol@prueba.com>
2023-10-16 19:13:07 +0200

    modified:   index.php 	new file:   view/cabecera.php 	new file:   view/panelPedido.php

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <?php
-        echo 'Esto es tu indicie';
+        if (isset($_GET['controller'])){
+            echo 'quieres realizar una accion sobre'.$_GET['controller'];
+            if (isset($_GET['action'])) {
+                echo 'Sobre'.$_GET['controller'].'quieres mostrar la pagina'.$_GET['action'];
+            }else{
+                echo 'No me has pasado controller';
+            }
+        }
+        
     ?>
     
 </body>
---
+++ view/panelPedido.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'este es tu panel de pedido';
+?>
