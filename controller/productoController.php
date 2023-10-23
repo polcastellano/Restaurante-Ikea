fa54278e339b4319cec09a551ca0da6b44d58def
Pol <polcastellanofp@ibf.cat>
2023-10-23 20:58:09 +0200

    modificacion panelPedido

--- controller/productoController.php
+++ controller/productoController.php
@@ -8,49 +8,16 @@ include_once 'model/ProductoDAO.php';
 class productoController{
 
     public function index(){
+        
+        
         //cabecera
         
         // panel include
-
-        echo "<h1>Platos Principales</h1>".
-        "<table border=1 style='text-align: center;'>".
-        "<th>Producto id</th>".
-        "<th>Categoria id</th>".
-        "<th>Nombre</th>".
-        "<th>Precio</th>";
-
         $platos = ProductoDAO::getAllPlatos();
-        foreach($platos as $plato){
-            
-            echo "<tr>".
-            "<td>".$plato->getProducto_id()."</td>".
-            "<td>".$plato->getCategoria_id()."</td>".
-            "<td>".$plato->getNombre()."</td>".
-            "<td>".$plato->getPrecio()."€</td>".
-            "</tr>";
-        }
-        echo "</table>";
-
-        echo "<br><br>";
-
-        echo "<h1>Desayunos</h1>".
-        "<table border=1 style='text-align: center;'>".
-        "<th>Producto id</th>".
-        "<th>Categoria id</th>".
-        "<th>Nombre</th>".
-        "<th>Precio</th>";
 
         $desayunos = ProductoDAO::getAllDesayunos();
-        foreach($desayunos as $desayuno){
 
-            echo "<tr>".
-            "<td>".$desayuno->getProducto_id()."</td>".
-            "<td>".$desayuno->getCategoria_id()."</td>".
-            "<td>".$desayuno->getNombre()."</td>".
-            "<td>".$desayuno->getPrecio()."€</td>".
-            "</tr>";
-        }
-        echo "</table>";
+        include_once 'view/panelPedido.php';
 
         //footer
 
--- view/panelPedido.php
+++ view/panelPedido.php
@@ -1,3 +1,50 @@
-<?php
-    echo 'este es tu panel de pedido';
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Panel Pedido</title>
+</head>
+<body>
+<h1>Productos</h1>
+    <h2>Platos Principales</h2>
+        <table border=1 style='text-align: center;'>
+        <th>Producto id</th>
+        <th>Categoria id</th>
+        <th>Nombre</th>
+        <th>Precio</th>
+
+        <?php foreach($platos as $plato){ ?>
+            
+            <tr>
+            <td><?=$plato->getProducto_id()?></td>
+            <td><?=$plato->getCategoria_id()?></td>
+            <td><?=$plato->getNombre()?></td>
+            <td><?=$plato->getPrecio()?></td>
+            <td><a href="modificarProducto.php">Modificar</a></td>
+            <td><a href="eliminarProducto.php">Eliminar</a></td>
+            </tr>
+        <?php } ?>
+        </table>
+
+    <h2>Platos Principales</h2>
+        <table border=1 style='text-align: center;'>
+        <th>Producto id</th>
+        <th>Categoria id</th>
+        <th>Nombre</th>
+        <th>Precio</th>
+
+        <?php foreach($desayunos as $desayuno){ ?>
+            
+            <tr>
+            <td><?=$desayuno->getProducto_id()?></td>
+            <td><?=$desayuno->getCategoria_id()?></td>
+            <td><?=$desayuno->getNombre()?></td>
+            <td><?=$desayuno->getPrecio()?></td>
+            <td><a href="modificarProducto.php">Modificar</a></td>
+            <td><a href="eliminarProducto.php">Eliminar</a></td>
+            </tr>
+        <?php } ?>
+        </table>
+</body>
+</html>
+
