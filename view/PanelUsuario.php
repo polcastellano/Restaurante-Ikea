49848429055af6de459973d02c1d0d4fe998626c
Pol <polcastellanofp@ibf.cat>
2024-01-06 10:22:09 +0100

    falta comentarios

--- view/PanelUsuario.php
+++ view/PanelUsuario.php
@@ -9,7 +9,7 @@
                 <div class="col-4 col-md-3">
                     <ul class="list-group">
                         <a href="" class="link-dark link-underline-opacity-0 link-underline-opacity-100-hover">
-                            <div class="elementoLista p-3">
+                            <div class="elementoLista1 p-3">
                                 <h3 class="listaPanelUsuario m-0 align-items-center d-flex">
                                     <svg width="22px" heigth="22px" viewBox="0 0 24 24" class="ocultos me-2">
                                         <path d="M16.8907 8.5235A5.966 5.966 0 0 1 17.917 11H20v2h-2.083a5.9674 5.9674 0 0 1-1.0263 2.4766l1.4731 1.4732-1.4142 1.4142-1.4731-1.4732A5.9679 5.9679 0 0 1 13 17.917V20h-2v-2.083a5.966 5.966 0 0 1-2.4765-1.0263l-1.4731 1.4731-1.4142-1.4142 1.473-1.4731A5.9675 5.9675 0 0 1 6.083 13H4v-2h2.083a5.9679 5.9679 0 0 1 1.0263-2.4766L5.6362 7.0502 7.0504 5.636l1.4731 1.4732A5.968 5.968 0 0 1 11 6.083V4h2v2.083a5.9675 5.9675 0 0 1 2.4765 1.0263l1.4731-1.4731 1.4142 1.4142-1.4731 1.473zM12 16c2.2091 0 4-1.7909 4-4 0-2.2091-1.7909-4-4-4-2.2091 0-4 1.7909-4 4 0 2.2091 1.7909 4 4 4z"></path>
@@ -135,43 +135,44 @@
             <?php } ?>
 
             <h2 class="textosTitulo mt-5 mb-5">Todos los pedidos</h2>
-            <div class="table-responsive">
-                <table class="table text-center mb-5">
-                    <thead>
-                        <tr class="align-middleS">
-                            <th class="osultos">Identificador pedido</th>
-                            <th>Fecha</th>
-                            <th>Precio total</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody class="justify-content-center align-items-center">
-                        <?php foreach ($pedidos as $pedido) { ?>
-                            <tr class="align-middle">
-                                <td class="ocultos align-middle"><?=$pedido->getPedido_id()?></td>
-                                <td class="align-middle"><?=$pedido->getFecha()?></td>
-                                <td class="align-middle"><?=CalculadoraPrecios::formatPrecios($pedido->getPrecio_total())?> €</td>
-                                <td class="align-middle">
-                                    <form action="<?=url."?controller=usuario&action=verPedido"?>" method="POST">
-                                        <input type="text" name="pedido_id" value="<?=$pedido->getPedido_id()?>" hidden>
-                                        <button type="submit" class="btn btn-primary btnVerPedido">Ver pedido</button>
-                                    </form>
-                                </td>    
-                                <td class="align-middle">
-                                    <form action="<?=url."?controller=usuario&action=borrarPedido"?>" method="POST">
-                                        <input type="text" name="pedido_id" value="<?=$pedido->getPedido_id()?>" hidden>
-                                        <button type="submit" class="btn btn-primary btnEliminarPedido">Borrar pedido</button>
-                                    </form>
-                                </td> 
+            <?php if(empty($pedidos)){ ?>
+                <p>Todavía no hay pedidos</p>
+            <?php }else{ ?>
+                <div class="table-responsive">
+                    <table class="table text-center mb-5">
+                        <thead>
+                            <tr class="align-middleS">
+                                <th class="osultos">Identificador pedido</th>
+                                <th>Fecha</th>
+                                <th>Precio total</th>
+                                <th></th>
+                                <th></th>
                             </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-
-
-            
+                        </thead>
+                        <tbody class="justify-content-center align-items-center">
+                            <?php foreach ($pedidos as $pedido) { ?>
+                                <tr class="align-middle">
+                                    <td class="ocultos align-middle"><?=$pedido->getPedido_id()?></td>
+                                    <td class="align-middle"><?=$pedido->getFecha()?></td>
+                                    <td class="align-middle"><?=CalculadoraPrecios::formatPrecios($pedido->getPrecio_total())?> €</td>
+                                    <td class="align-middle">
+                                        <form action="<?=url."?controller=usuario&action=verPedido"?>" method="POST">
+                                            <input type="text" name="pedido_id" value="<?=$pedido->getPedido_id()?>" hidden>
+                                            <button type="submit" class="btn btn-primary btnVerPedido">Ver pedido</button>
+                                        </form>
+                                    </td>    
+                                    <td class="align-middle">
+                                        <form action="<?=url."?controller=usuario&action=borrarPedido"?>" method="POST">
+                                            <input type="text" name="pedido_id" value="<?=$pedido->getPedido_id()?>" hidden>
+                                            <button type="submit" class="btn btn-primary btnEliminarPedido">Borrar pedido</button>
+                                        </form>
+                                    </td> 
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            <?php } ?>            
         </div>
     </main>
 </body>
