2f0cf9dfada1b4850e2d7764c4e4d2c5772ff663
Pol <polcastellanofp@ibf.cat>
2024-01-28 10:16:41 +0100

    correccion de filtros reseñas

--- assets/js/reseñas.js
+++ assets/js/reseñas.js
@@ -1,58 +1,79 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-        fetch("http://localhost/DAW/ikea/?controller=api&action=consultaReseñas", {
-            method : 'POST',
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            mostrarReseñas(data);
-        })
-        .catch(error => {
-            console.error(error);
+    // Obtén el formulario por su ID o cualquier otro selector
+    let formulario = document.getElementById('filtroReseña');
+
+    let starContainer = document.getElementById('estrellas');
+    let stars = starContainer.querySelectorAll('.star-icon');
+
+    let seleccionado;
+
+    stars.forEach(function(star, index) {
+
+        star.addEventListener('click', function() {
+        resetStars(stars);
+        markStars(index);
+        star.classList.add('active');
+        document.getElementById('valoracion').value = index + 1; // Asigna el valor de la estrella al input hidden
+        seleccionado = index;
         });
-});
 
+        document.getElementsByClassName('hoverEstrellas')[index].addEventListener('mouseover', function() {
+            resetStars(stars);
+            markStars(this.id);
+        });
 
-function mostrarReseñas(reseñas){
-    let sectionReseñas = document.getElementById('resenas');
-    sectionReseñas.classList.add("col-8");
-
-    reseñas.forEach ((reseña) => {
-        let article = document.createElement('article');
-        let div2 = document.createElement('div');
-        let div3 = document.createElement('div');
-        let titulo = document.createElement('h5');
-        let pComentario = document.createElement('p');
-
-        article.classList.add("card", "rounded-0", "border-0", "border-bottom", "mb-3");
-        div2.classList.add("card-body", "ps-0");
-
-        div3.classList.add("d-flex");
-        div3.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="black" >
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.6724 6.4678c.2734-.2812.6804-.4707 1.3493-.4707.3971 0 .705.0838.9529.2225.241.1348.4379.3311.5934.6193l.0033.006c.1394.2541.237.6185.237 1.1403 0 .7856-.2046 1.2451-.4796 1.5278l-.0048.005c-.2759.2876-.679.4764-1.334.4764-.3857 0-.6962-.082-.956-.2241-.2388-.1344-.4342-.3293-.5888-.6147-.1454-.275-.2419-.652-.2419-1.1704 0-.7902.2035-1.2442.4692-1.5174zm1.3493-2.4717c-1.0834 0-2.054.3262-2.7838 1.0766-.7376.7583-1.0358 1.781-1.0358 2.9125 0 .7656.1431 1.483.4773 2.112l.0031.0058c.3249.602.785 1.084 1.3777 1.4154l.0062.0035c.5874.323 1.2368.4736 1.9235.4736 1.0818 0 2.0484-.3333 2.7755-1.0896.7406-.7627 1.044-1.786 1.044-2.9207 0-.7629-.1421-1.4784-.482-2.0996-.3247-.6006-.7844-1.0815-1.376-1.4125-.5858-.3276-1.2388-.477-1.9297-.477zM6.4691 16.8582c.2983-.5803.7228-1.0273 1.29-1.3572.5582-.3191 1.2834-.5049 2.2209-.5049h4.04c.9375 0 1.6626.1858 2.2209.5049.5672.3299.9917.7769 1.29 1.3572.3031.5896.4691 1.2936.4691 2.1379v1h2v-1c0-1.1122-.2205-2.1384-.6904-3.0523a5.3218 5.3218 0 0 0-2.0722-2.1769c-.9279-.5315-2.0157-.7708-3.2174-.7708H9.98c-1.1145 0-2.2483.212-3.2225.7737-.8982.5215-1.5928 1.2515-2.0671 2.174C4.2205 16.8577 4 17.8839 4 18.9961v1h2v-1c0-.8443.166-1.5483.4691-2.1379z"></path>
-                            </svg>`;
-
-        titulo.classList.add("card-title", "ms-2");
-        titulo.textContent = reseña.nombre_usuario;
-
-        pComentario.classList.add("card-text");
-        pComentario.textContent = `Comentario: ${reseña.comentario}`;
-
-        let estrellasValoracion = generarValoracion(reseña.valoracion);
-
-        sectionReseñas.appendChild(article);
-        article.appendChild(div2);
-        div2.appendChild(div3);
-        div3.appendChild(titulo);
-        div2.appendChild(estrellasValoracion);
-        div2.appendChild(pComentario);        
+        document.getElementsByClassName('hoverEstrellas')[index].addEventListener('mouseout', function() {
+            if(seleccionado == null){
+                resetStars(stars);  
+            }else{
+                markStars(index);
+            }
+        });
     });
 
-    let filtros = generarFiltros();
-    let contenedor = document.getElementById('contenidoPagina');
-    contenedor.appendChild(filtros);
+    function markStars(index) {
+        for (let i = 0; i < index; i++) {
+            stars[i].classList.add('active');
+        }
+    }
+
+    let orders = document.querySelectorAll('.iconosOrden');
+
+    orders.forEach(function(order) {
+        order.addEventListener('click', function() {
+            // Eliminar la clase 'active' de todos los elementos
+            orders.forEach(function(otherOrder) {
+                otherOrder.classList.remove('active');
+            });
+
+            // Agregar la clase 'active' solo al elemento clicado
+            order.classList.add('active');
+        });
+    });
+
+    fetch("http://localhost/DAW/ikea/?controller=api&action=consultaReseñas", {
+        method : 'POST',
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        mostrarReseñas(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+});
+
+//Array para guardar las reseñas y luego ordenar y filtrar estas mismas
+let arrayReseñas;
+
+function mostrarReseñas(reseñas){
+    arrayReseñas = reseñas;
+    let sectionReseñas = document.getElementById('resenas');
+    montarReseñas(reseñas);
+    
 };
 
 function generarValoracion(valoracion){
@@ -90,60 +111,70 @@ function generarValoracion(valoracion){
     return div;
 };
 
-function generarFiltros(){
+function asignarEstrella(numero){
+    let sectionReseñas = document.getElementById('resenas');
+    while (sectionReseñas.firstChild) {
+        sectionReseñas.removeChild(sectionReseñas.firstChild);
+    }
     
-    let filtros = document.createElement('section');
+    let arrayFiltrado = arrayReseñas.filter((reseña) => reseña.valoracion == numero);
+    montarReseñas(arrayFiltrado);
+};
 
-    filtros.classList.add("col-4");
-
-    filtros.innerHTML = `
-                    <div class="elementoLista1 p-3">
-                        <h3 class="filtrarReseñaTitulo m-0 align-items-center d-flex">Filtrar reseñas</h3>
-                    </div>
-    
-    
-                    <form id="filtroReseña" method="POST">
-                    <div class="col">
-                        <div id="estrellas" class="d-flex mt-4 p-0">
-
-                            <input id="valoracion" hidden />
-
-                            <div class="mb-3" onclick="asignarEstrella(1)">
-                                <svg width="20" height="20" class="star-icon">
-                                    <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-                                </svg>
-                            </div>
-                            <div class="mb-3" onclick="asignarEstrella(2)">
-                                <svg width="20" height="20" class="star-icon">
-                                    <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-                                </svg>
-                            </div>
-                            <div class="mb-3" onclick="asignarEstrella(3)">
-                                <svg width="20" height="20" class="star-icon">
-                                    <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-                                </svg>
-                            </div>
-                            <div class="mb-3" onclick="asignarEstrella(4)">
-                                <svg width="20" height="20" class="star-icon">
-                                    <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-                                </svg>
-                            </div>
-                            <div class="mb-3" onclick="asignarEstrella(5)">
-                                <svg width="20" height="20" class="star-icon">
-                                    <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-    
-    
-    
-                    `;
-
-    return filtros;
+//Reset de las estrellas para limpiar las estrellas cuando filtro por orden
+function resetStars(stars) {
+    stars.forEach(function(star) {
+        star.classList.remove('active');
+    });
 }
 
-function asignarEstrella(numero){
-    valoracion = numero;
+function asignarOrden(cadena){
+
+    let sectionReseñas = document.getElementById('resenas');
+    while (sectionReseñas.firstChild) {
+        sectionReseñas.removeChild(sectionReseñas.firstChild);
+    }
+    if (cadena == 'asc'){
+        let arrayOrdenado = arrayReseñas.sort((a, d) => d.valoracion - a.valoracion);
+        montarReseñas(arrayOrdenado);
+    }else if (cadena == 'desc'){
+        let arrayOrdenado = arrayReseñas.sort((a, d) => a.valoracion - d.valoracion);
+        montarReseñas(arrayOrdenado);
+    }
+    
 };
+
+function montarReseñas(reseñas){
+    let sectionReseñas = document.getElementById('resenas');
+
+    reseñas.forEach ((reseña) => {
+        let article = document.createElement('article');
+        let div2 = document.createElement('div');
+        let div3 = document.createElement('div');
+        let titulo = document.createElement('h5');
+        let pComentario = document.createElement('p');
+
+        article.classList.add("card", "rounded-0", "border-0", "border-bottom", "mb-3");
+        div2.classList.add("card-body", "ps-0");
+
+        div3.classList.add("d-flex");
+        div3.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="black" >
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.6724 6.4678c.2734-.2812.6804-.4707 1.3493-.4707.3971 0 .705.0838.9529.2225.241.1348.4379.3311.5934.6193l.0033.006c.1394.2541.237.6185.237 1.1403 0 .7856-.2046 1.2451-.4796 1.5278l-.0048.005c-.2759.2876-.679.4764-1.334.4764-.3857 0-.6962-.082-.956-.2241-.2388-.1344-.4342-.3293-.5888-.6147-.1454-.275-.2419-.652-.2419-1.1704 0-.7902.2035-1.2442.4692-1.5174zm1.3493-2.4717c-1.0834 0-2.054.3262-2.7838 1.0766-.7376.7583-1.0358 1.781-1.0358 2.9125 0 .7656.1431 1.483.4773 2.112l.0031.0058c.3249.602.785 1.084 1.3777 1.4154l.0062.0035c.5874.323 1.2368.4736 1.9235.4736 1.0818 0 2.0484-.3333 2.7755-1.0896.7406-.7627 1.044-1.786 1.044-2.9207 0-.7629-.1421-1.4784-.482-2.0996-.3247-.6006-.7844-1.0815-1.376-1.4125-.5858-.3276-1.2388-.477-1.9297-.477zM6.4691 16.8582c.2983-.5803.7228-1.0273 1.29-1.3572.5582-.3191 1.2834-.5049 2.2209-.5049h4.04c.9375 0 1.6626.1858 2.2209.5049.5672.3299.9917.7769 1.29 1.3572.3031.5896.4691 1.2936.4691 2.1379v1h2v-1c0-1.1122-.2205-2.1384-.6904-3.0523a5.3218 5.3218 0 0 0-2.0722-2.1769c-.9279-.5315-2.0157-.7708-3.2174-.7708H9.98c-1.1145 0-2.2483.212-3.2225.7737-.8982.5215-1.5928 1.2515-2.0671 2.174C4.2205 16.8577 4 17.8839 4 18.9961v1h2v-1c0-.8443.166-1.5483.4691-2.1379z"></path>
+                            </svg>`;
+
+        titulo.classList.add("card-title", "ms-2");
+        titulo.textContent = reseña.nombre_usuario;
+
+        pComentario.classList.add("card-text");
+        pComentario.textContent = `Comentario: ${reseña.comentario}`;
+
+        let estrellasValoracion = generarValoracion(reseña.valoracion);
+
+        sectionReseñas.appendChild(article);
+        article.appendChild(div2);
+        div2.appendChild(div3);
+        div3.appendChild(titulo);
+        div2.appendChild(estrellasValoracion);
+        div2.appendChild(pComentario);        
+    });
+}
--- view/panelResenas.php
+++ view/panelResenas.php
@@ -2,8 +2,66 @@
         <div class="contenido">
             <h2 class="textosTitulo mt-5 mb-5">Reseñas</h2>
             <div id="contenidoPagina" class="d-flex">
-                <section id="resenas">
+                <section class="col-6 col-md-8" id="resenas">
 
+                </section>
+                <section class="col ps-3">
+                    <div class="elementoLista1 p-3">
+                        <h3 class="filtrarReseñaTitulo m-0 align-items-center d-flex">Filtrar reseñas</h3>
+                    </div>
+        
+        
+                    <form id="filtroReseña" method="POST">
+                        <div class="row m-0 col-4">
+                            <label class="mt-4 p-0">Valoración:</label>
+                            <div id="estrellas" class="d-flex mb-4 p-0">
+
+                                <input id="valoracion" hidden />
+                                
+                                <div id="1" class="hoverEstrellas" onclick="asignarEstrella(1)">
+                                    <svg width="20" height="20" class="star-icon">
+                                        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+                                    </svg>
+                                </div>
+                                <div id="2" class="hoverEstrellas" onclick="asignarEstrella(2)">
+                                    <svg width="20" height="20" class="star-icon">
+                                        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+                                    </svg>
+                                </div>
+                                <div id="3" class="hoverEstrellas" onclick="asignarEstrella(3)">
+                                    <svg width="20" height="20" class="star-icon">
+                                        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+                                    </svg>
+                                </div>
+                                <div id="4" class="hoverEstrellas" onclick="asignarEstrella(4)">
+                                    <svg width="20" height="20" class="star-icon">
+                                        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+                                    </svg>
+                                </div>
+                                <div id="5" class="hoverEstrellas" onclick="asignarEstrella(5)">
+                                    <svg width="20" height="20" class="star-icon">
+                                        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                            <label class="mt-4 p-0">Ordenar:</label>
+                            <div id="ordenacion" class="d-flex mb-4 p-0">
+
+                                <input id="orden" hidden />
+
+                                <div class="rounded-circle iconosOrden d-flex justify-content-center align-items-center" onclick="asignarOrden('asc')">
+                                    <svg width="24" height="24">
+                                        <path d="M11.9999 17.0605 3.9992 9.0593l1.4142-1.4141L12 14.2322l6.5869-6.586 1.4141 1.4143-8.0011 8z"></path>
+                                    </svg>
+                                </div>
+                                <div class="ms-3 rounded-circle iconosOrden d-flex justify-content-center align-items-center" onclick="asignarOrden('desc')">
+                                    <svg width="24" height="24">
+                                        <path d="m12.0001 6.9394 8.0007 8.0013-1.4142 1.4141L12 9.7678l-6.5869 6.586-1.414-1.4143 8.001-8z"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </section>
             </div>
         </div>
@@ -11,5 +69,4 @@
 
         
 <script src="./assets/js/reseñas.js"></script>
-<script src="./assets/js/filtrarReseña.js"></script>
 </body>
